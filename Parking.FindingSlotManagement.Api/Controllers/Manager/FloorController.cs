5561ff039ad2d84041ad00c4ba9032353a7dd4d0
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-04-28 01:03:59 +0700

    Dat: Done CRUD API Floor Entity For Manager

---
+++ Parking.FindingSlotManagement.Api/Controllers/Manager/FloorController.cs
@@ -0,0 +1,150 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Parking.FindingSlotManagement.Application;
+using Parking.FindingSlotManagement.Application.Features.Manager.Floors.FloorManagement.Commands.CreateNewFloor;
+using Parking.FindingSlotManagement.Application.Features.Manager.Floors.FloorManagement.Commands.DisableOrEnableFloor;
+using Parking.FindingSlotManagement.Application.Features.Manager.Floors.FloorManagement.Commands.UpdateFloor;
+using Parking.FindingSlotManagement.Application.Features.Manager.Floors.FloorManagement.Queries.GetListFloor;
+using Parking.FindingSlotManagement.Infrastructure.Hubs;
+using System.Net;
+
+namespace Parking.FindingSlotManagement.Api.Controllers.Manager
+{
+    [Route("api/floors")]
+    [ApiController]
+    public class FloorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IHubContext<MessageHub> _messageHub;
+
+        public FloorController(IMediator mediator, IHubContext<MessageHub> messageHub)
+        {
+            _mediator = mediator;
+            _messageHub = messageHub;
+        }
+        /// <summary>
+        /// API For Manager
+        /// </summary>
+        [HttpGet(Name = "GetListFloor")]
+        [Produces("application/json")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ServiceResponse<IEnumerable<GetListFloorResponse>>>> GetListFloor([FromQuery] int pageNo, [FromQuery] int pageSize)
+        {
+            try
+            {
+                var query = new GetListFloorQuery()
+                {
+                    PageNo = pageNo,
+                    PageSize = pageSize
+                };
+                var res = await _mediator.Send(query);
+                return StatusCode((int)res.StatusCode, res);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// API For Manager
+        /// </summary>
+        /// <remarks>
+        /// SignalR: LoadFloorInManager
+        /// </remarks>
+        [HttpPost("floor", Name = "CreateNewFloor")]
+        [Produces("application/json")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<ServiceResponse<int>>> CreateNewFloor([FromBody] CreateNewFloorCommand command)
+        {
+            try
+            {
+                var res = await _mediator.Send(command);
+                if (res.Message == "Thành công")
+                {
+                    await _messageHub.Clients.All.SendAsync("LoadFloorInManager");
+                    return StatusCode((int)res.StatusCode, res);
+                }
+                return StatusCode((int)res.StatusCode, res);
+            }
+            catch (Exception ex)
+            {
+                IEnumerable<string> list1 = new List<string> { "Severity: Error" };
+                string message = "";
+                foreach (var item in list1)
+                {
+                    message = ex.Message.Replace(item, string.Empty);
+                }
+                var errorResponse = new ErrorResponseModel(ResponseCode.BadRequest, "Validation Error: " + message.Remove(0, 31));
+                return StatusCode((int)ResponseCode.BadRequest, errorResponse);
+            }
+        }
+        /// <summary>
+        /// API For Manager
+        /// </summary>
+        /// <remarks>
+        /// SignalR: LoadFloorInManager
+        /// </remarks>
+        [HttpPut("floor/{floorId}", Name = "UpdateFloor")]
+        [Produces("application/json")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<ServiceResponse<string>>> UpdateFloor(int floorId, [FromBody] UpdateFloorCommand command)
+        {
+            try
+            {
+                var res = await _mediator.Send(command);
+                if (res.Message != "Thành công")
+                {
+                    return StatusCode((int)res.StatusCode, res);
+                }
+                await _messageHub.Clients.All.SendAsync("LoadFloorInManager");
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                IEnumerable<string> list1 = new List<string> { "Severity: Error" };
+                string message = "";
+                foreach (var item in list1)
+                {
+                    message = ex.Message.Replace(item, string.Empty);
+                }
+                var errorResponse = new ErrorResponseModel(ResponseCode.BadRequest, "Validation Error: " + message.Remove(0, 31));
+                return StatusCode((int)ResponseCode.BadRequest, errorResponse);
+            }
+        }
+        /// <summary>
+        /// API For Manager
+        /// </summary>
+        /// <remarks>
+        /// SignalR: LoadFloorInManager
+        /// </remarks>
+        [HttpDelete("floor/{floorId}", Name = "DisableOrEnableFloor")]
+        [Produces("application/json")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<ServiceResponse<string>>> DisableOrEnableFloor(int floorId)
+        {
+            try
+            {
+                var command = new DisableOrEnableFloorCommand() { FloorId = floorId };
+                var res = await _mediator.Send(command);
+                if (res.Message != "Thành công")
+                {
+                    return StatusCode((int)res.StatusCode, res);
+                }
+                await _messageHub.Clients.All.SendAsync("LoadFloorInManager");
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application/Features/Manager/Floors/FloorManagement/Commands/DisableOrEnableFloor/DisableOrEnableFloorCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Manager.Floors.FloorManagement.Commands.DisableOrEnableFloor
+{
+    public class DisableOrEnableFloorCommand : IRequest<ServiceResponse<string>>
+    {
+        public int FloorId { get; set; }
+    }
+}
