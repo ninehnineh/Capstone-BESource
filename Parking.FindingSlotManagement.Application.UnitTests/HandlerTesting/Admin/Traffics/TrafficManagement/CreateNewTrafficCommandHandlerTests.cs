5bcefc6e218ce66891a30a54b1be8e1dafef6f6d
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-05-03 14:33:50 +0700

    Dat: Unit-Test: Add Unit Test For Traffic Entity in Admin

---
+++ Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/Admin/Traffics/TrafficManagement/CreateNewTrafficCommandHandlerTests.cs
@@ -0,0 +1,113 @@
+﻿using AutoMapper;
+using FluentValidation.TestHelper;
+using Moq;
+using Parking.FindingSlotManagement.Application.Contracts.Persistence;
+using Parking.FindingSlotManagement.Application.Features.Admin.Traffics.TrafficManagement.Commands.CreateNewTraffic;
+using Parking.FindingSlotManagement.Application.Mapping;
+using Parking.FindingSlotManagement.Domain.Entities;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.Admin.Traffics.TrafficManagement
+{
+    public class CreateNewTrafficCommandHandlerTests
+    {
+        private readonly Mock<ITrafficRepository> _trafficRepositoryMock;
+        private readonly CreateNewTrafficCommandHandler _handler;
+        private readonly CreateNewTrafficCommandValidation _validator;
+        private readonly IMapper _mapper;
+        MapperConfiguration configuration;
+
+        public CreateNewTrafficCommandHandlerTests()
+        {
+            _trafficRepositoryMock = new Mock<ITrafficRepository>();
+            _handler = new CreateNewTrafficCommandHandler(_trafficRepositoryMock.Object);
+            _validator = new CreateNewTrafficCommandValidation();
+            configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            _mapper = configuration.CreateMapper();
+        }
+        [Fact]
+        public async Task Handle_WhenCreatedSuccessfully_ReturnsCreated()
+        {
+            // Arrange
+            var request = new CreateNewTrafficCommand { Name = "Xe container" };
+            var expectedTraffic = new User { Name = "Xe container" };
+            _trafficRepositoryMock.Setup(x => x.GetItemWithCondition(It.IsAny<Expression<Func<Traffic, bool>>>(), null, true))
+                .ReturnsAsync((Traffic)null);
+
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(201);
+            response.Success.ShouldBeTrue();
+            response.Count.ShouldBe(0);
+            response.Message.ShouldBe("Thành công");
+            // Verify that the account repository was called to insert the new account
+            _trafficRepositoryMock.Verify(x => x.Insert(It.Is<Traffic>(traffic => traffic.Name == expectedTraffic.Name)));
+        }
+        [Fact]
+        public async Task Handle_WhenTrafficExists_ReturnsBadRequest()
+        {
+            // Arrange
+            var request = new CreateNewTrafficCommand { Name = "Xe ô tô" };
+            var existingTraffic = new Traffic { Name = "Xe ô tô" };
+            _trafficRepositoryMock.Setup(x => x.GetItemWithCondition(It.IsAny<Expression<Func<Traffic, bool>>>(), null, true))
+                .ReturnsAsync(existingTraffic);
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(400);
+            response.Success.ShouldBeFalse();
+            response.Count.ShouldBe(0);
+            response.Message.ShouldBe("Tên phương tiện đã tồn tại. Vui lòng nhập lại tên phương tiện!!!");
+            
+        }
+        [Fact]
+        public void Name_ShouldNotBeEmpty()
+        {
+            var command = new CreateNewTrafficCommand
+            {
+                Name = "",
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+        [Fact]
+        public void Name_ShouldNotBeNull()
+        {
+            var command = new CreateNewTrafficCommand();
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+        [Fact]
+        public void Name_ShouldNotExceedMaximumLength()
+        {
+            var command = new CreateNewTrafficCommand
+            {
+                Name = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/Admin/Traffics/TrafficManagement/UpdateTrafficCommandHandlerTests.cs
@@ -0,0 +1,129 @@
+﻿using FluentValidation.TestHelper;
+using Moq;
+using Parking.FindingSlotManagement.Application.Contracts.Persistence;
+using Parking.FindingSlotManagement.Application.Features.Admin.Traffics.TrafficManagement.Commands.UpdateTraffic;
+using Parking.FindingSlotManagement.Domain.Entities;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.Admin.Traffics.TrafficManagement
+{
+    public class UpdateTrafficCommandHandlerTests
+    {
+        private readonly Mock<ITrafficRepository> _trafficRepositoryMock;
+        private readonly UpdateTrafficCommandHandler _handler;
+        private readonly UpdateTrafficCommandValidation _validator;
+
+        public UpdateTrafficCommandHandlerTests()
+        {
+            _trafficRepositoryMock = new Mock<ITrafficRepository>();
+            _handler = new UpdateTrafficCommandHandler(_trafficRepositoryMock.Object);
+            _validator = new UpdateTrafficCommandValidation();
+        }
+        [Fact]
+        public async Task UpdateTrafficCommandHandler_Should_Update_Traffic_Successfully()
+        {
+            // Arrange
+            var request = new UpdateTrafficCommand
+            {
+                TrafficId = 1,
+                Name = "Xe container"
+            };
+            var cancellationToken = new CancellationToken();
+            var Oldtraffic = new Traffic
+            {
+                TrafficId = 1,
+                Name = "Xe ô tô",
+                IsActive = true,
+            };
+            _trafficRepositoryMock.Setup(x => x.GetById(request.TrafficId))
+                .ReturnsAsync(Oldtraffic);
+
+            // Act
+            var response = await _handler.Handle(request, cancellationToken);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeTrue();
+            response.Message.ShouldBe("Thành công");
+            response.StatusCode.ShouldBe(204);
+            response.Count.ShouldBe(0);
+            Oldtraffic.Name.ShouldBe(request.Name);
+            _trafficRepositoryMock.Verify(x => x.Update(Oldtraffic), Times.Once);
+        }
+        [Fact]
+        public async Task UpdateTrafficCommandHandler_Should_Return_Not_Found_If_Traffic_Does_Not_Exist()
+        {
+            // Arrange
+            var request = new UpdateTrafficCommand
+            {
+                TrafficId = 2000
+            };
+            var cancellationToken = new CancellationToken();
+            _trafficRepositoryMock.Setup(x => x.GetById(request.TrafficId))
+                .ReturnsAsync((Traffic)null);
+
+            // Act
+            var response = await _handler.Handle(request, cancellationToken);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeTrue();
+            response.Message.ShouldBe("Không tìm thấy.");
+            response.StatusCode.ShouldBe(200);
+            response.Count.ShouldBe(0);
+            _trafficRepositoryMock.Verify(x => x.Update(It.IsAny<Traffic>()), Times.Never);
+        }
+        [Fact]
+        public async Task UpdateTrafficCommandHandler_Should_Return_Name_Already_Exists()
+        {
+            // Arrange
+            var command = new UpdateTrafficCommand
+            {
+                TrafficId = 1,
+                Name = "Xe máy", // This name already exists
+                
+            };
+            _trafficRepositoryMock.Setup(x => x.GetById(command.TrafficId))
+                .ReturnsAsync(new Traffic
+                {
+                    TrafficId = command.TrafficId,
+                    Name = "Xe ô tô" // Existing email for the account
+                });
+            _trafficRepositoryMock.Setup(x => x.GetItemWithCondition(It.IsAny<Expression<Func<Traffic, bool>>>(), null, true))
+                .ReturnsAsync(new Traffic
+                {
+                    TrafficId = 1,
+                    Name = command.Name // Name already exists for another traffic
+                });
+
+            // Act
+            var response = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            response.Success.ShouldBeTrue();
+            response.StatusCode.ShouldBe(200);
+            response.Count.ShouldBe(0);
+            response.Message.ShouldBe("Tên phương tiện đã tồn tại. Hãy nhập tên khác!!!");
+            _trafficRepositoryMock.Verify(x => x.Update(It.IsAny<Traffic>()), Times.Never);
+        }
+        [Fact]
+        public void Name_ShouldNotExceedMaximumLength()
+        {
+            var command = new UpdateTrafficCommand
+            {
+                TrafficId = 1,
+                Name = "lorem loremloremlorem lorem lorem lorem lorem lorem lorem lorem loremv lorem lorem lorem",
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+    }
+}
