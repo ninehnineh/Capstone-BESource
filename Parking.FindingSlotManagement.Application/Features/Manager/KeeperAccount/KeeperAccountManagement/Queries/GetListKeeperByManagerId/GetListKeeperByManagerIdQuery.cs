0632d4b0ff59fd9565744cb7fd2618493852b77c
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-06-14 02:49:00 +0700

    Dat: Done Create, GetList and GetById API keeper by Manager and keeper

---
+++ Parking.FindingSlotManagement.Application/Features/Manager/KeeperAccount/KeeperAccountManagement/Queries/GetListKeeperByManagerId/GetListKeeperByManagerIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Manager.KeeperAccount.KeeperAccountManagement.Queries.GetListKeeperByManagerId
+{
+    public class GetListKeeperByManagerIdQuery : IRequest<ServiceResponse<IEnumerable<GetListKeeperByManagerIdResponse>>>
+    {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application/Features/Manager/KeeperAccount/KeeperAccountManagement/Queries/GetListKeeperByManagerId/GetListKeeperByManagerIdResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Manager.KeeperAccount.KeeperAccountManagement.Queries.GetListKeeperByManagerId
+{
+    public class GetListKeeperByManagerIdResponse
+    {
+        public int UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Avatar { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Phone { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
