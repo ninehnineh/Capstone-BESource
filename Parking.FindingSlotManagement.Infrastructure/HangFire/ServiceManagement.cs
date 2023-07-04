e326d6da3f18511172f35ad1126ebe9c76ba65b6
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-04 11:29:47 +0700

    Dat: Done Get Available slot api

--- Parking.FindingSlotManagement.Infrastructure/HangFire/ServiceManagement.cs
+++ Parking.FindingSlotManagement.Infrastructure/HangFire/ServiceManagement.cs
@@ -1,4 +1,6 @@
-﻿using Parking.FindingSlotManagement.Application.Contracts.Infrastructure;
+﻿using Hangfire;
+using Parking.FindingSlotManagement.Application.Contracts.Infrastructure;
+using Parking.FindingSlotManagement.Domain.Entities;
 using Parking.FindingSlotManagement.Infrastructure.Persistences;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,43 @@ namespace Parking.FindingSlotManagement.Infrastructure.HangFire
         {
             _context = context;
         }
+
+        public void AddTimeSlotInFuture(int floorId)
+        {
+            var lstParkingSlot = _context.ParkingSlots.Where(x => x.FloorId == floorId).ToList();
+            DateTime startDate = DateTime.UtcNow;
+            DateTime endDate = startDate.AddDays(7);
+            
+            foreach (var a in lstParkingSlot)
+            {
+                List<TimeSlot> ts = new List<TimeSlot>();
+                for (DateTime date = startDate; date < endDate; date = date.AddDays(1))
+                {
+                    for (int i = 0; i < 24; i++)
+                    {
+                        DateTime startTime = date.Date + TimeSpan.FromHours(i);
+                        DateTime endTime = date.Date + TimeSpan.FromHours(i + 1);
+
+                        var entityTimeSlot = new TimeSlot
+                        {
+                            StartTime = startTime,
+                            EndTime = endTime,
+                            CreatedDate = date.Date,
+                            Status = "Free",
+                            ParkingSlotId = a.ParkingSlotId
+                        };
+                        ts.Add(entityTimeSlot);
+                    }
+                }
+                _context.TimeSlots.AddRange(ts);
+                _context.SaveChanges();
+            }
+            Console.WriteLine($"Add TimeSlot In Future: Long running task {DateTime.UtcNow.AddHours(7).ToString("yyyy-MM-dd HH:mm:ss")}");
+            RecurringJob.AddOrUpdate<IServiceManagement>(x => x.DeleteTimeSlotIn1Week(), Cron.Weekly);
+            RecurringJob.AddOrUpdate<IServiceManagement>(x => x.AddTimeSlotInFuture((int)floorId), Cron.Weekly);
+
+        }
+
         public void DeleteTimeSlotIn1Week()
         {
             var oneWeekAgo = DateTime.UtcNow.AddDays(-7);
