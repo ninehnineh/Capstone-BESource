0463de27e1536be80f61046679c4a3beae98735c
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-07-05 20:34:53 +0700

    Chinh: Migration add unpaidmoney to booking

---
+++ Parking.FindingSlotManagement.Infrastructure/Migrations/20230705132914_AddNewPropToBooking.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parking.FindingSlotManagement.Infrastructure.Migrations
+{
+    public partial class AddNewPropToBooking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Parking");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnPaidMoney",
+                table: "Booking",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnPaidMoney",
+                table: "Booking");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Parking",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+    }
+}
