7ca67a51521d6994111640385bfb4b55ad848ccb
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-07-12 15:58:54 +0700

    Chinh: Fix background Job cancel booking

--- Parking.FindingSlotManagement.Application/Features/Manager/Booking/Commands/ApproveBooking/ApproveBookingCommandHandler.cs
+++ Parking.FindingSlotManagement.Application/Features/Manager/Booking/Commands/ApproveBooking/ApproveBookingCommandHandler.cs
@@ -43,7 +43,6 @@ namespace Parking.FindingSlotManagement.Application.Features.Manager.Booking.Com
         public async Task<ServiceResponse<string>> Handle(ApproveBookingCommand request, CancellationToken cancellationToken)
         {
             var bookingId = request.BookingId;
-            var business_Representatives = request.BusinessId;
             try
             {
 
@@ -72,12 +71,12 @@ namespace Parking.FindingSlotManagement.Application.Features.Manager.Booking.Com
 
                 if (isPostPaid)
                 {
-                    var timeToCancel = booking.StartTime.AddHours(1);
+                    var timeToCancel = booking.StartTime.AddHours(1) - DateTime.UtcNow.AddHours(7);
                     BackgroundJob.Schedule<IServiceManagement>(x => x.AutoCancelBookingWhenOverAllowTimeBooking(bookingId), timeToCancel);
                 }
                 else if (isPrePaid)
                 {
-                    var timeToCancel = booking.EndTime.Value;
+                    var timeToCancel = booking.EndTime.Value - DateTime.UtcNow.AddHours(7);
                     BackgroundJob.Schedule<IServiceManagement>(x => x.AutoCancelBookingWhenOutOfEndTimeBooking(bookingId), timeToCancel);
                 }
 
