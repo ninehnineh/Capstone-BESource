8b4fb533a8ffe03d70d9b221549b477da4afb138
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-12 12:39:03 +0700

    Dat: Done API: GetListCustomer, GetAllParking, GetInforOfBusinessByManagerId For Admin

---
+++ Parking.FindingSlotManagement.Application/Features/Admin/ParkingManagement/Queries/GetAllParkingForAdmin/GetAllParkingForAdminQueryHandler.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using MediatR;
+using Parking.FindingSlotManagement.Application.Contracts.Persistence;
+using Parking.FindingSlotManagement.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Admin.ParkingManagement.Queries.GetAllParkingForAdmin
+{
+    public class GetAllParkingForAdminQueryHandler : IRequestHandler<GetAllParkingForAdminQuery, ServiceResponse<IEnumerable<GetAllParkingForAdminResponse>>>
+    {
+        private readonly IParkingRepository _parkingRepository;
+        MapperConfiguration config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new MappingProfile());
+        });
+        public GetAllParkingForAdminQueryHandler(IParkingRepository parkingRepository)
+        {
+            _parkingRepository = parkingRepository;
+        }
+        public async Task<ServiceResponse<IEnumerable<GetAllParkingForAdminResponse>>> Handle(GetAllParkingForAdminQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (request.PageNo <= 0)
+                {
+                    request.PageNo = 1;
+                }
+                if (request.PageSize <= 0)
+                {
+                    request.PageSize = 1;
+                }
+                var lst = await _parkingRepository.GetAllItemWithPagination(null, null, x => x.ParkingId, true, request.PageNo, request.PageSize);
+                var _mapper = config.CreateMapper();
+                var lstDto = _mapper.Map<IEnumerable<GetAllParkingForAdminResponse>>(lst);
+                if (lstDto.Count() <= 0)
+                {
+                    return new ServiceResponse<IEnumerable<GetAllParkingForAdminResponse>>
+                    {
+                        Message = "Không tìm thấy bãi giữ xe.",
+                        Success = true,
+                        StatusCode = 200
+                    };
+                }
+                return new ServiceResponse<IEnumerable<GetAllParkingForAdminResponse>>
+                {
+                    Data = lstDto,
+                    Success = true,
+                    StatusCode = 200,
+                    Message = "Thành công",
+                    Count = lstDto.Count()
+                };
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application/Features/Manager/BusinessProfile/BusinessProfileManagement/Queries/GetInforOfBusinessByManagerId/GetInforOfBusinessByManagerIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Manager.BusinessProfile.BusinessProfileManagement.Queries.GetInforOfBusinessByManagerId
+{
+    public class GetInforOfBusinessByManagerIdQuery : IRequest<ServiceResponse<GetInforOfBusinessByManagerIdResponse>>
+    {
+        public int ManagerId { get; set; }
+    }
+}
