69ab6958e5b84129aa007e0616caa505edb0577d
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-30 20:02:40 +0700

    Done: Additional and modify unit test for approve parking in admin

--- Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/Admin/ApproveParking/GetFieldInforByParkingIdQueryHandlerTests.cs
+++ Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/Admin/ApproveParking/GetFieldInforByParkingIdQueryHandlerTests.cs
@@ -1,6 +1,8 @@
-﻿/*using Moq;
+﻿using AutoMapper;
+using Moq;
 using Parking.FindingSlotManagement.Application.Contracts.Persistence;
 using Parking.FindingSlotManagement.Application.Features.Admin.ApproveParking.Queries.GetFieldInforByParkingId;
+using Parking.FindingSlotManagement.Domain.Entities;
 using Shouldly;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,7 @@ namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.Adm
     {
         private readonly Mock<IApproveParkingRepository> _approveParkingRepositoryMock;
         private readonly Mock<IParkingRepository> _parkingRepositoryMock;
+        private readonly Mock<IMapper> _mapper;
         private readonly Mock<IFieldWorkParkingImgRepository> _fieldWorkParkingImgRepositoryMock;
         private readonly GetFieldInforByParkingIdQueryHandler _handler;
         public GetFieldInforByParkingIdQueryHandlerTests()
@@ -22,93 +25,70 @@ namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.Adm
             _approveParkingRepositoryMock = new Mock<IApproveParkingRepository>();
             _parkingRepositoryMock = new Mock<IParkingRepository>();
             _fieldWorkParkingImgRepositoryMock = new Mock<IFieldWorkParkingImgRepository>();
-            _handler = new GetFieldInforByParkingIdQueryHandler(_parkingRepositoryMock.Object, _approveParkingRepositoryMock.Object, _fieldWorkParkingImgRepositoryMock.Object);
+            _mapper = new Mock<IMapper>();
+            _handler = new GetFieldInforByParkingIdQueryHandler(_parkingRepositoryMock.Object, _mapper.Object, _approveParkingRepositoryMock.Object, _fieldWorkParkingImgRepositoryMock.Object);
         }
         [Fact]
-        public async Task Handle_WhenParkingDoesNotExist_ShouldReturnNotFoundResponse()
+        public async Task Handle_WhenParkingDoesNotExist_ReturnsNotFoundResponse()
         {
             // Arrange
-            var parkingId = 1;
-
-            _parkingRepositoryMock.Setup(r => r.GetById(parkingId)).ReturnsAsync((Domain.Entities.Parking)null);
-
-
-            var query = new GetFieldInforByParkingIdQuery { ParkingId = parkingId };
+            var request = new GetFieldInforByParkingIdQuery { ParkingId = 1 };
+            _parkingRepositoryMock.Setup(x => x.GetById(1)).ReturnsAsync((Domain.Entities.Parking)null);
 
             // Act
-            var response = await _handler.Handle(query, CancellationToken.None);
+            var response = await _handler.Handle(request, CancellationToken.None);
 
             // Assert
             response.ShouldNotBeNull();
             response.Success.ShouldBeTrue();
-            response.Message.ShouldBe("Không tìm thấy bãi giữ xe.");
             response.StatusCode.ShouldBe(200);
-            response.Data.ShouldBeNull();
+            response.Message.ShouldBe("Không tìm thấy bãi giữ xe.");
         }
         [Fact]
-        public async Task Handle_WhenNoImagesExist_ShouldReturnBadRequestResponse()
+        public async Task Handle_WhenApproveParkingIsEmpty_ReturnsBadRequestResponse()
         {
             // Arrange
-            var parkingId = 1;
-            var parkingExist = new Domain.Entities.Parking { ParkingId = parkingId };
-            var approveParkingInfo = new Domain.Entities.ApproveParking { ApproveParkingId = 1 };
-
-            _parkingRepositoryMock.Setup(r => r.GetById(parkingId)).ReturnsAsync(parkingExist);
-
-            _approveParkingRepositoryMock.Setup(r => r.GetItemWithCondition(It.IsAny<Expression<Func<Domain.Entities.ApproveParking, bool>>>(), It.IsAny<List<Expression<Func<Domain.Entities.ApproveParking, object>>>>(), true))
-                .ReturnsAsync(approveParkingInfo);
-
-            _fieldWorkParkingImgRepositoryMock.Setup(r => r.GetAllItemWithConditionByNoInclude(It.IsAny <Expression<Func<Domain.Entities.FieldWorkParkingImg, bool>>> ()))
-                .ReturnsAsync(new List<Domain.Entities.FieldWorkParkingImg>());
-
-
-            var query = new GetFieldInforByParkingIdQuery { ParkingId = parkingId };
+            var request = new GetFieldInforByParkingIdQuery { ParkingId = 1 };
+            var parking = new Domain.Entities.Parking { ParkingId = 1 };
+            _parkingRepositoryMock.Setup(x => x.GetById(1)).ReturnsAsync(parking);
+            _approveParkingRepositoryMock.Setup(x => x.GetAllItemWithCondition(It.IsAny<Expression<Func<Domain.Entities.ApproveParking, bool>>>(), It.IsAny<List<Expression<Func<Domain.Entities.ApproveParking, object>>>>(), null, true)).ReturnsAsync(new List<Domain.Entities.ApproveParking>());
 
             // Act
-            var response = await _handler.Handle(query, CancellationToken.None);
+            var response = await _handler.Handle(request, CancellationToken.None);
 
             // Assert
             response.ShouldNotBeNull();
             response.Success.ShouldBeFalse();
-            response.Message.ShouldBe("Chưa có hình ảnh thực địa.");
             response.StatusCode.ShouldBe(400);
-            response.Data.ShouldBeNull();
+            response.Message.ShouldBe("Chưa có thông tin thực địa.");
         }
         [Fact]
-        public async Task Handle_WhenAllConditionsAreMet_ShouldReturnSuccessResponseWithFieldInformation()
+        public async Task Handle_WhenApproveParkingIsNotEmpty_ReturnsSuccessResponse()
         {
             // Arrange
-            var parkingId = 1;
-            var parkingExist = new Domain.Entities.Parking { ParkingId = parkingId };
-            var approveParkingInfo = new Domain.Entities.ApproveParking { ApproveParkingId = 1, Note = "Some note", StaffId = 1, User = new Domain.Entities.User { Name = "John Doe" } };
-            var images = new List<Domain.Entities.FieldWorkParkingImg> { new Domain.Entities.FieldWorkParkingImg { Url = "image1.jpg" }, new Domain.Entities.FieldWorkParkingImg { Url = "image2.jpg" } };
+            var request = new GetFieldInforByParkingIdQuery { ParkingId = 1 };
+            var parking = new Domain.Entities.Parking { ParkingId = 1 };
+            var approveParkingList = new List<Domain.Entities.ApproveParking>
+        {
+            new Domain.Entities.ApproveParking { ApproveParkingId = 1, ParkingId = 1, Status = "Đã_duyệt", StaffId = 101, User = new User { UserId = 101, Name = "John Doe" } },
+            new Domain.Entities.ApproveParking { ApproveParkingId = 2, ParkingId = 1, Status = "Chờ_duyệt", StaffId = 102, User = new User { UserId = 102, Name = "Jane Smith" } },
+        };
 
-            _parkingRepositoryMock.Setup(r => r.GetById(parkingId)).ReturnsAsync(parkingExist);
-
-            _approveParkingRepositoryMock.Setup(r => r.GetItemWithCondition(It.IsAny<Expression<Func<Domain.Entities.ApproveParking, bool>>>(), It.IsAny<List<Expression<Func<Domain.Entities.ApproveParking, object>>>>(),true))
-                .ReturnsAsync(approveParkingInfo);
-
-            _fieldWorkParkingImgRepositoryMock.Setup(r => r.GetAllItemWithConditionByNoInclude(It.IsAny < Expression<Func<Domain.Entities.FieldWorkParkingImg, bool> >> ()))
-                .ReturnsAsync(images);
-
-
-            var query = new GetFieldInforByParkingIdQuery { ParkingId = parkingId };
+            _parkingRepositoryMock.Setup(x => x.GetById(1)).ReturnsAsync(parking);
+            _approveParkingRepositoryMock.Setup(x => x.GetAllItemWithCondition(It.IsAny<Expression<Func<Domain.Entities.ApproveParking, bool>>>(), It.IsAny<List<Expression<Func<Domain.Entities.ApproveParking, object>>>>(), null, true)).ReturnsAsync(approveParkingList);
 
             // Act
-            var response = await _handler.Handle(query, CancellationToken.None);
+            var response = await _handler.Handle(request, CancellationToken.None);
 
             // Assert
             response.ShouldNotBeNull();
             response.Success.ShouldBeTrue();
-            response.Message.ShouldBe("Thành công");
             response.StatusCode.ShouldBe(200);
+            response.Message.ShouldBe("Thành công");
             response.Data.ShouldNotBeNull();
-            response.Data.ApproveParkingId.ShouldBe(approveParkingInfo.ApproveParkingId);
-            response.Data.Note.ShouldBe(approveParkingInfo.Note);
-            response.Data.StaffId.ShouldBe(approveParkingInfo.StaffId);
-            response.Data.StaffName.ShouldBe(approveParkingInfo.User.Name);
-            response.Data.Images.ShouldBe(images.Select(img => img.Url));
+            response.Data.Count().ShouldBe(2);
+            response.Data.ElementAt(0).StaffName.ShouldBe("John Doe");
+            response.Data.ElementAt(1).StaffName.ShouldBe("Jane Smith");
         }
     }
 }
-*/
