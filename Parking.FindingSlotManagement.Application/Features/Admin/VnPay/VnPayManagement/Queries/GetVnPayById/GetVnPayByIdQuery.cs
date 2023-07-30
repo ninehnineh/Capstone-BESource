52acd55a3147a643f073c455b3c1e0e0751469c0
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-30 10:36:41 +0700

    Dat: Done API getVnpayById

---
+++ Parking.FindingSlotManagement.Application/Features/Admin/VnPay/VnPayManagement/Queries/GetVnPayById/GetVnPayByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Admin.VnPay.VnPayManagement.Queries.GetVnPayById
+{
+    public class GetVnPayByIdQuery : IRequest<ServiceResponse<GetVnPayByIdResponse>>
+    {
+        public int VnPayId { get; set; }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application/Features/Admin/VnPay/VnPayManagement/Queries/GetVnPayById/GetVnPayByIdResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.Features.Admin.VnPay.VnPayManagement.Queries.GetVnPayById
+{
+    public class GetVnPayByIdResponse
+    {
+        public int VnPayId { get; set; }
+        public string? TmnCode { get; set; }
+        public string? HashSecret { get; set; }
+    }
+}
