0292d2ce8ff86d2d8acc7a6ac0a3c8d3f1a329e7
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-31 09:28:57 +0700

    Dat: Additional and modified unit test: AutoCancel and Common API

---
+++ Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/AutoCancel/AutomationCancelBookingCommandHandlerTest.cs
@@ -0,0 +1,83 @@
+﻿using Moq;
+using Parking.FindingSlotManagement.Application.Contracts.Persistence;
+using Parking.FindingSlotManagement.Application.Features.AutoCancel.commands.AutomationCancelBooking;
+using Parking.FindingSlotManagement.Domain.Entities;
+using Parking.FindingSlotManagement.Domain.Enum;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.AutoCancel
+{
+    public class AutomationCancelBookingCommandHandlerTest
+    {
+        private readonly Mock<IBookingRepository> _bookingRepositoryMock;
+        private readonly Mock<ITimeSlotRepository> _timeSlotRepositoryMock;
+        private readonly AutomationCancelBookingCommandHandler _handler;
+        public AutomationCancelBookingCommandHandlerTest()
+        {
+            _bookingRepositoryMock = new Mock<IBookingRepository>();
+            _timeSlotRepositoryMock = new Mock<ITimeSlotRepository>();
+            _handler = new AutomationCancelBookingCommandHandler(_bookingRepositoryMock.Object, _timeSlotRepositoryMock.Object);
+        }
+        [Fact]
+        public async Task Handle_WhenBookingDoesNotExist_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var request = new AutomationCancelBookingCommand { BookingId = 1 };
+            _bookingRepositoryMock.Setup(repo => repo.GetBookingIncludeTimeSlot(1))
+                                  .ReturnsAsync((Domain.Entities.Booking)null);
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeTrue();
+            response.StatusCode.ShouldBe(200);
+            response.Message.ShouldBe("Đơn không tồn tại");
+        }
+        [Fact]
+        public async Task Handle_WhenBookingExistsWithNullCheckinTime_CancelsBookingAndFreesTimeSlots()
+        {
+            // Arrange
+            var request = new AutomationCancelBookingCommand { BookingId = 1 };
+            var booking = new Domain.Entities.Booking
+            {
+                BookingId = 1,
+                CheckinTime = null,
+                Status = BookingStatus.Success.ToString(),
+                BookingDetails = new List<Domain.Entities.BookingDetails>
+                {
+                    new Domain.Entities.BookingDetails { TimeSlot = new TimeSlot { Status = TimeSlotStatus.Booked.ToString() } },
+                new Domain.Entities.BookingDetails { TimeSlot = new TimeSlot { Status = TimeSlotStatus.Booked.ToString() } }
+            }
+            };
+            _bookingRepositoryMock.Setup(repo => repo.GetBookingIncludeTimeSlot(1))
+                                  .ReturnsAsync(booking);
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeTrue();
+            response.StatusCode.ShouldBe(200);
+            response.Message.ShouldBe("Thành công");
+
+            // Check that the booking was canceled and time slots were freed
+            booking.Status.ShouldBe(BookingStatus.Cancel.ToString());
+            foreach (var item in booking.BookingDetails)
+            {
+                item.TimeSlot.Status.ShouldBe(TimeSlotStatus.Free.ToString());
+            }
+
+            // Check that Save method was called for both repositories
+            _bookingRepositoryMock.Verify(repo => repo.Save(), Times.Once);
+            _timeSlotRepositoryMock.Verify(repo => repo.Save(), Times.Once);
+        }
+    }
+}
---
+++ Parking.FindingSlotManagement.Application.UnitTests/HandlerTesting/Common/WalletManagement/DepositToAWalletCommandHandlerTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Parking.FindingSlotManagement.Application.Contracts.Infrastructure;
+using Parking.FindingSlotManagement.Application.Contracts.Persistence;
+using Parking.FindingSlotManagement.Application.Features.Common.WalletManagement.Commands.DepositToAWallet;
+using Parking.FindingSlotManagement.Application.Models.Wallet;
+using Parking.FindingSlotManagement.Domain.Entities;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.FindingSlotManagement.Application.UnitTests.HandlerTesting.Common.WalletManagement
+{
+    public class DepositToAWalletCommandHandlerTest
+    {
+        private readonly Mock<IWalletRepository> _walletRepositoryMock;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IConfiguration> _iConfigurationMock;
+        private readonly Mock<IVnPayService> _vnPayServiceMock;
+        private readonly DepositToAWalletCommandHandler _handler;
+        public DepositToAWalletCommandHandlerTest()
+        {
+            _walletRepositoryMock = new Mock<IWalletRepository>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _iConfigurationMock = new Mock<IConfiguration>();
+            _vnPayServiceMock = new Mock<IVnPayService>();
+            _handler = new DepositToAWalletCommandHandler(_walletRepositoryMock.Object, _userRepositoryMock.Object, _iConfigurationMock.Object, _vnPayServiceMock.Object);
+        }
+        [Fact]
+        public async Task Handle_WhenUserDoesNotExist_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var request = new DepositToAWalletCommand { UserId = 1, TotalPrice = 100 };
+            _userRepositoryMock.Setup(repo => repo.GetItemWithCondition(It.IsAny<Expression<Func<User, bool>>>(), It.IsAny<List<Expression<Func<User, object>>>>(), true))
+                               .ReturnsAsync((User)null);
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeTrue();
+            response.StatusCode.ShouldBe(200);
+            response.Message.ShouldBe("Không tìm thấy tài khoản hoặc tài khoản đã bị hệ thống ban.");
+            response.Data.ShouldBeNull();
+        }
+        [Fact]
+        public async Task Handle_WhenUserIsNotAllowedToDeposit_ReturnsUnauthorizedResponse()
+        {
+            // Arrange
+            var request = new DepositToAWalletCommand { UserId = 1, TotalPrice = 100 };
+            var user = new User { UserId = 1, RoleId = 4, IsActive = true }; // RoleId 4 means unauthorized user
+            _userRepositoryMock.Setup(repo => repo.GetItemWithCondition(It.IsAny<Expression<Func<User, bool>>>(), It.IsAny<List<Expression<Func<User, object>>>>(), true))
+                               .ReturnsAsync(user);
+
+            // Act
+            var response = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            response.ShouldNotBeNull();
+            response.Success.ShouldBeFalse();
+            response.StatusCode.ShouldBe(400);
+            response.Message.ShouldBe("Tài khoản của bạn không được phép nạp tiền vào ví.");
+            response.Data.ShouldBeNull();
+        }
+    }
+}
